432be21b8e34a0505c0693c72356d22b4b403585
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2023-12-05 13:18:53 -0300

    read_files completo

--- OS.py
+++ OS.py
@@ -1,7 +1,7 @@
 from __future__ import annotations 
 
 import auxiliar
-from auxiliar import read_processes
+from auxiliar import read_processes, read_files
 
 import processos
 from processos import Processo
@@ -16,8 +16,31 @@ def main() -> None:
 
 	lista_de_processos = read_processes(memoria)
 
+	print("conteudo do arquivo processes.txt:")
 	for p in lista_de_processos:
 		print(p)
+
+
+	list_file_operations: list[dict[str, str | int] ] = []
+	arquivos_na_memoria: list[dict[str, str | int] ] = []
+
+	blocos_disco:int = \
+					read_files(list_file_operations, arquivos_na_memoria)
+
+	print(f"Numero de blocos: {blocos_disco}")
+	print("conteudo do arquivo files.txt:------------------")
+
+	print("lista de arquivos na memoria: ")
+	for arquivo in arquivos_na_memoria:
+		print(arquivo)
+
+	print("lista de operacoes nos arquivos: ")
+	for file_operation in list_file_operations:
+		print(file_operation)
+
+
+
+
 
 if __name__ == "__main__":
 	main()
--- auxiliar.py
+++ auxiliar.py
@@ -39,3 +39,57 @@ def read_processes(memoria: Memoria) ->list[Processo]:
 
 
 	return lista_de_processos
+
+
+def read_files(list_file_operations: list[ dict[str, str | int] ],\
+				arquivos_na_memoria: list[ dict[str, str | int] ]) -> int:
+
+	file = open("files.txt")
+
+	lines = file.readlines()
+
+	blocos_disco:int = int(lines[0])
+
+	segmentos_ocupados:int = int(lines[1])
+
+	for i in range(2, segmentos_ocupados+2):
+		line:str = lines[i]
+
+		new_line:list[str] = line.split(", ")
+		new_line[2] = new_line[2].replace("\n","")
+
+		arquivo_na_memoria: dict[str, str | int ] = dict()
+
+		arquivo_na_memoria["nome_do_arquivo"] = new_line[0]
+		arquivo_na_memoria["bloco_inicial"] = int(new_line[1])
+		arquivo_na_memoria["blocos_ocupados"] = int(new_line[2])
+
+		arquivos_na_memoria.append(arquivo_na_memoria)
+
+
+	linha_inicial_operacoes:int = segmentos_ocupados+2
+
+	for i in range(linha_inicial_operacoes, len(lines)):
+		line = lines[i]
+
+		new_line = line.split(", ")
+		new_line[-1] = new_line[-1].replace("\n","")
+
+		nova_operacao: dict[str, str | int] = dict()
+
+		nova_operacao["id"] = int(new_line[0])
+		nova_operacao["codigo_operacao"] = int(new_line[1])
+		nova_operacao["nome_arquivo"] = new_line[2]
+
+		if (nova_operacao["codigo_operacao"] == 0):
+			nova_operacao["quantidade_blocos"] = new_line[3]
+
+		list_file_operations.append(nova_operacao)
+
+
+
+
+
+
+
+	return blocos_disco
